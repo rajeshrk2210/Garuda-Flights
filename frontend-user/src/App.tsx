b1a090a40159179d9a8a64e6b91fdb0e0677316c
Rajesh Kumar <rajesh.rk2210@gmail.com>
2025-03-05 15:13:05 -0500

    frontend-user Login and SignUp

---
+++ frontend-user/src/App.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
+const App = () => {
+  return (
+    <div className="w-full min-h-screen flex flex-col">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
